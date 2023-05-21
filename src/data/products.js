926cbfee5ee1d51ebfd20d98e0521662bcc2e918
David Kiraly <david@Davids-MBP.home>
2023-05-21 21:49:27 +0200

    add code

---
+++ src/data/products.js
@@ -0,0 +1,65 @@
+const products = [
+    {
+      id: 1,
+      name: "Red T-Shirt",
+      price: 19.99,
+      salePrice: 14.99,
+      description: "A comfortable red t-shirt for casual wear.",
+      quantity: 10,
+      category: "Apparel",
+      image: "https://source.unsplash.com/featured/?red,t-shirt"
+    },
+    {
+      id: 2,
+      name: "Blue Jeans",
+      price: 49.99,
+      salePrice: null,
+      description: "Classic blue jeans for everyday use.",
+      quantity: 5,
+      category: "Apparel",
+      image: "https://source.unsplash.com/featured/?blue,jeans"
+    },
+    {
+      id: 3,
+      name: "Running Shoes",
+      price: 89.99,
+      salePrice: 79.99,
+      description: "High-performance running shoes for athletes.",
+      quantity: 8,
+      category: "Footwear",
+      image: "https://source.unsplash.com/featured/?running,shoes"
+    },
+    {
+      id: 4,
+      name: "Leather Handbag",
+      price: 129.99,
+      salePrice: null,
+      description: "A stylish leather handbag for fashion enthusiasts.",
+      quantity: 3,
+      category: "Accessories",
+      image: "https://source.unsplash.com/featured/?leather,handbag"
+    },
+    {
+      id: 5,
+      name: "Smartphone",
+      price: 599.99,
+      salePrice: null,
+      description: "The latest smartphone with advanced features.",
+      quantity: 12,
+      category: "Electronics",
+      image: "https://source.unsplash.com/featured/?smartphone"
+    },
+    {
+      id: 6,
+      name: "Coffee Maker",
+      price: 39.99,
+      salePrice: 34.99,
+      description: "An efficient coffee maker for brewing your favorite beverages.",
+      quantity: 6,
+      category: "Kitchen",
+      image: "https://source.unsplash.com/featured/?coffee,maker"
+    }
+  ];
+  
+  export default products;
+  
