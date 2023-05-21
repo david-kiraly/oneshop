e9729f7af64ed0326ffb7582f3f23aad6fe27536
David Kiraly <david@Davids-MBP.home>
2023-05-21 22:12:32 +0200

    remove homepage

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,19 +6,17 @@ import React from 'react';
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="bg-indigo-900">
+      {/* <header className="bg-indigo-900">
         <div className="container mx-auto px-4 py-6">
-          {/* Your header content */}
         </div>
-      </header>
+      </header> */}
       <main className="flex-grow bg-gray-100">
         {children}
       </main>
-      <footer className="bg-gray-900 text-white">
+      {/* <footer className="bg-gray-900 text-white">
         <div className="container mx-auto px-4 py-6">
-          {/* Your footer content */}
         </div>
-      </footer>
+      </footer> */}
     </div>
   );
 };
